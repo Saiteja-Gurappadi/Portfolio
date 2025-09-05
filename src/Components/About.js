4badf7647b089ddde49efb3c973b400b85775b9a
Saiteja-Gurappadi <saitejagvs8@gmail.com>
2025-09-05 15:39:55 +0530

    About Changed

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,20 @@
-import React from "react";
-
+import Vector186 from "../Images/Vector 186.png";
+import Group2346 from "../Images/Group 62.png";
+import doodleitems from "../Images/doodle items.png";
 const About = () => {
-  return <div className="flex-column h-screen bg-slate-900 relative"></div>;
+  return (
+    <div className="flex h-screen bg-slate-900 relative p-5 items-center justify-evenly">
+      <div className="h-[400px] w-[300px] text-white text-[18px] font-semibold" >
+        <h1 className="text-bold text-[45px] text-center font-poppins">ABOUT <span className="text-cyan-400">ME</span></h1>
+        <p>HI I am G.V.S.Saiteja I have handson experince on React and i have built some projects using.......................... </p>
+        <img src={Vector186} alt=""  className=""/>
+      </div>
+      <div className="flex h-[75%] w-[35%] relative">
+        <img src={Group2346} alt="" className="h-[60%] w-[60%] absolute bottom-[0%] left-[15%]" />
+        <img src={doodleitems} alt=""  className="h-[80%] w-[100%]" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,18 @@
-import React from 'react'
 import vector from '../Images/Vector 187.png';
 import mixedround from '../Images/doodles mixed round.png';
 import group from '../Images/Group 2346.png';
+import { PiArrowDownThin } from "react-icons/pi";
 
 const Home = () => {
   return (
           <div className="flex-column h-screen bg-slate-900 relative">
-        <div className='absolute left-[30%] h-[100%] border-l border-solid border-slate-800 z-1 '>
-          <h1 className='h-[80px] w-[10px] text-white'>
-↓</h1>
+        <div className='absolute left-[30%] h-[95%] border-l border-solid border-slate-800 z-1 '>
+          
         </div>
         <div className='w-[100%] text-white h-[15%] border-b border-solid border-slate-800   px-[70px] '>
         <div className='flex w-[100%] h-[100%] justify-between items-center '>
           <div className='flex items-center'>
-            <h1 className='font-oswald font-bold  text-[18px]'>SaiTeja's Design</h1>
+            
           </div>
           <div className='font-poppins flex items-center justify-between w-[30%]'>
               <p className='hover:text-cyan-400 transition cursor-pointer'>Home</p>
@@ -26,13 +25,13 @@ const Home = () => {
       <div className='flex items-center gap-[120px] justify-center mt-[70px] z-10 relative '>
           <div className='flex gap-[5px]'>
             <div className='h-[270px] flex items-center'>
-              <img src={vector} alt='' className='h-[160px] w-[30px]'/>
+              <img src={vector} alt='' className='h-[190px] w-[60px]'/>
             </div>
-            <div className='flex flex-col gap-[40px] h-[250px] justify-center'>
-              <h1 className='text-white text-5xl font-bold flex flex-col gap-[15px]'> CREATIVE UI <br/><span className='text-cyan-400'>
-                DESIGNER</span> </h1>
+            <div className='flex flex-col gap-[30px] h-[250px] justify-center'>
+              <h1 className='text-white text-6xl font-bold flex flex-col gap-[15px] font-poppins'> UI/UX <br/><span className='text-cyan-400'>
+                DEVELOPER</span> </h1>
               <div className='flex items-center gap-[10px]'>
-                <button className='bg-cyan-500 text-white font-semibold px-6 py-2 rounded-full font-medium hover:bg-cyan-400 transition'>Hire Me</button>
+                <button className='bg-cyan-500 text-white font-semibold px-6 py-2 rounded-full font-medium hover:bg-cyan-400 transition '>Hire Me</button>
                 <button className='bg-slate-800 text-white font-semibold px-6 py-2 rounded-full font-medium hover:bg-slate-700 transition' >Download CV</button>
               </div>
               
@@ -43,7 +42,10 @@ const Home = () => {
             <img src={group} alt='' className='absolute z-1 top-[140px] right-[110px]'/>
           </div>
       </div>
+        <p className='p-[11px] py-[13px] bg-slate-800 text-white text-center absolute left-[28%] bottom-[14%] text-4xl'>< PiArrowDownThin/> 
 
+</p>
+      
     </div>
   )
 }
